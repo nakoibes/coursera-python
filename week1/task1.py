58daf0e1ddc1c2efe9597ee508c89b615dbc74b9
nakoibes <alefanow.d@yandex.ru>
2021-03-26 15:42:37 +0300

    pomenyal

--- week1/task1.py
+++ week1/task1.py
@@ -1,16 +1,23 @@
 import sys
 
 
-def digit_sum(digit_string):
+def convert_string_to_list(input_data: str) -> list[int]:
+    good_data = []
+    for digit in input_data:
+        good_data.append(digit)
+    return good_data
+
+
+def digit_sum(digit_list: list[int]) -> int:
     result = 0
-    for digit in digit_string:
+    for digit in digit_list:
         result += int(digit)
     return result
 
 
 def main():
     digit_string = sys.argv[1]
-    print(digit_sum(digit_string))
+    print(digit_sum(convert_string_to_list(digit_string)))
 
 
 if __name__ == '__main__':
