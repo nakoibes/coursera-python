2a8b5007e6337e7f285f02f46b78ef6614634424
nakoibes <alefanow.d@yandex.ru>
2021-03-26 14:25:55 +0300

    task1

--- week1/task1.py
+++ week1/task1.py
@@ -1,6 +1,14 @@
 import sys
+
 digit_string = sys.argv[1]
-result = 0
-for digit in digit_string:
-    result += int(digit)
-print(result)
+
+
+def digit_sum(digit_string):
+    result = 0
+    for digit in digit_string:
+        result += int(digit)
+    return result
+
+
+if __name__ == '__main__':
+    print(digit_sum(digit_string))
