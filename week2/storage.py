b870fb54613bb5d9fd0f249e1743a981d0443325
nakoibes <alefanow.d@yandex.ru>
2021-03-29 17:18:07 +0300

    storage

--- week2/storage.py
+++ week2/storage.py
@@ -3,24 +3,24 @@ import os
 import tempfile
 
 
-def write_the_data(key: str, value: str) -> None:
+def write_the_data(key_data: str, value_data: str) -> None:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'a') as f:
-        f.write(key + ' ')
-        f.write(value + ' ')
+        f.write(key_data + ' ')
+        f.write(value_data + ' ')
 
 
-def get_values(key: str) -> list[str] or None:
-    dictionary = {}
+def get_values(key_data: str) -> list[str] or None:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'r') as f:
         line = f.read()
-        line = line.split(' ')
-        for index in range(0, len(line) - 1, 2):
-            dictionary.setdefault(line[index], [])
-            dictionary[line[index]].append(line[index + 1])
-    if dictionary[key]:
-        return dictionary[key]
+        line_list = line.split()
+        key_list = [line_list[index] for index in range(0, len(line_list), 2)]
+        key_set = set(key_list)
+        dictionary = {key: ([line_list[index + 1] for index in range(0, len(line_list), 2) if line_list[index] == key])
+                      for key in key_set}
+    if dictionary[key_data]:
+        return dictionary[key_data]
     else:
         return None
 
@@ -36,12 +36,12 @@ def get_data() -> list[str]:
 def main():
     try:
         data = get_data()
-        key = data[0]
-        value = data[1]
-        if key and value:
-            write_the_data(key, value)
-        elif key:
-            print(*get_values(key), sep=', ')
+        key_data = data[0]
+        value_data = data[1]
+        if key_data and value_data:
+            write_the_data(key_data, value_data)
+        elif key_data:
+            print(*get_values(key_data), sep=', ')
         else:
             print('No Data')
     except FileNotFoundError:
