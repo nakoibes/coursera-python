3ca4ff30fe9b5e6240abffbd3d3f86e7a40ff642
nakoibes <alefanow.d@yandex.ru>
2021-04-01 16:37:15 +0300

    Polushilos!

--- week2/storage.py
+++ week2/storage.py
@@ -4,50 +4,48 @@ import os
 import tempfile
 from typing import Optional
 
+STORAGE_PATH = os.path.join(tempfile.gettempdir(), 'storagee.data')
+
 
 def handle_command(key_data: Optional[str], value_data: Optional[str]) -> None:
     if key_data and value_data:
         write_the_data(key_data, value_data)
     elif key_data:
         print(', '.join(get_values(key_data)))
+    else:
+        print("Wrong command")
 
 
-def upadate_data(source_data: dict[str:list[str]], key: str, value: str) -> dict[str:list[str]]:
+def upadate_data(source_data: dict[str, list[str]], key: str, value: str) -> None:
     if key not in source_data:
         source_data.update({key: [value]})
     else:
         source_data[key].append(value)
-    return source_data
 
 
-def update_storage(data: dict[str:[str]], path: str) -> None:
-    with open(path, 'w') as f:
+def update_storage(data: dict[str, [str]]) -> None:
+    with open(STORAGE_PATH, 'w') as f:
         json.dump(data, f)
 
 
-def read_storage(path: str) -> dict[str:[str]]:
-    if os.path.exists(path):
-        with open(path, 'r') as f:
+def read_storage() -> dict[str, [str]]:
+    storage = {}
+    if os.path.exists(STORAGE_PATH):
+        with open(STORAGE_PATH, 'r') as f:
             file_content = f.read()
             if file_content:
-                f.seek(0)
-                return json.load(f)
-            else:
-                return {}
-    else:
-        return {}
+                storage.update(json.loads(file_content))
+    return storage
 
 
 def write_the_data(key_data: str, value_data: str) -> None:
-    storage_path = os.path.join(tempfile.gettempdir(), 'storagee.data')
-    data = read_storage(storage_path)
-    data = upadate_data(data, key_data, value_data)
-    update_storage(data, storage_path)
+    data = read_storage()
+    upadate_data(data, key_data, value_data)
+    update_storage(data)
 
 
 def get_values(key_data: str) -> list[str]:
-    storage_path = os.path.join(tempfile.gettempdir(), 'storagee.data')
-    data = read_storage(storage_path)
+    data = read_storage()
     return data.get(key_data, [])
 
 
