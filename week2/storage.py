d300a59f10a222c78eff004346b4920a3c0eaeb1
nakoibes <alefanow.d@yandex.ru>
2021-04-01 03:36:14 +0300

    Polushilos!

--- week2/storage.py
+++ week2/storage.py
@@ -1,29 +1,31 @@
 import argparse
+import json
 import os
 import tempfile
 
 
 def write_the_data(key_data: str, value_data: str) -> None:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    with open(storage_path, 'a'
-                            '') as f:
-        f.write(key_data + ' ')
-        f.write(value_data + ' ')
+    if os.stat(storage_path).st_size == 0:
+        with open(storage_path, 'a') as f:
+            json.dump({key_data: [value_data]}, f)
+    else:
+        with open(storage_path, 'r') as f:
+            data = json.load(f)
+            if key_data not in data:
+                data.update({key_data: [value_data]})
+            else:
+                data[key_data].append(value_data)
+
+        with open(storage_path, 'w') as f:
+            json.dump(data, f)
 
 
-def get_values(key_data: str) -> list[str] or None:
+def get_values(key_data: str) -> list[str]:
     storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
     with open(storage_path, 'r') as f:
-        line = f.read()
-        line_list = line.split()
-        key_list = [line_list[index] for index in range(0, len(line_list), 2)]
-        key_set = set(key_list)
-        dictionary = {key: [line_list[index + 1] for index in range(0, len(line_list), 2) if line_list[index] == key]
-                      for key in key_set}
-    if dictionary[key_data]:
-        return dictionary[key_data]
-    else:
-        return None
+        data = json.load(f).get(key_data, [])
+    return data
 
 
 def get_data() -> list[str]:
@@ -36,17 +38,13 @@ def get_data() -> list[str]:
 
 def main():
     try:
-        data = get_data()
-        key_data = data[0]
-        value_data = data[1]
+        key_data, value_data = get_data()
         if key_data and value_data:
             write_the_data(key_data, value_data)
         elif key_data:
-            print(*get_values(key_data), sep=', ')
+            print(', '.join(get_values(key_data)))
         else:
             print('No Data')
-    except FileNotFoundError:
-        print('There is no storage.data ')
     except KeyError:
         print('None')
 
