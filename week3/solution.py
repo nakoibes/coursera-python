94e8de5470459a6d3f18df5d155d0dca330ff9e2
nakoibes <alefanow.d@yandex.ru>
2021-04-01 20:47:34 +0300

    FileReader

--- week3/solution.py
+++ week3/solution.py
@@ -1,12 +1,16 @@
+from contextlib import suppress
+
+
 class FileReader:
     def __init__(self, path):
         self.path = path
 
     def read(self):
-        line = ''
-        try:
+        return self._read()
+
+    def _read(self):
+        with suppress(FileNotFoundError):
             with open(self.path, 'r') as f:
                 line = f.read()
                 return line
-        except FileNotFoundError as err:
-            return line
+        return ''
