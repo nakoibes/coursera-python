fb3d5ea19a0cb5c0ca61f83c935b3eb3bf445622
nakoibes <alefanow.d@yandex.ru>
2021-04-08 01:39:04 +0300

    commit

---
+++ week4/magiya.py
@@ -0,0 +1,53 @@
+import os
+import tempfile
+
+
+class File:
+    storage_path = os.path.join(tempfile.gettempdir(), 'result.txt')
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.counter = -1
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w'):
+                pass
+
+    def read(self):
+        with open(self.filename, 'r') as f:
+            return f.read()
+
+    def write(self, text):
+        with open(self.filename, 'w') as f:
+            f.write(text)
+
+    def writea(self, text):
+        with open(self.filename, 'a') as f:
+            f.write(text)
+
+    def __add__(self, other):
+        result = File(self.storage_path)
+        result.write(self.read())
+        result.writea(other.read())
+        return result
+
+    def __str__(self):
+        return os.path.join(os.getcwd(), self.filename)
+
+    def __iter__(self):
+        return self
+
+    def file_list(self):
+        a = []
+        with open(self.filename, 'r') as f:
+            for line in f.readlines():
+                a.append(line)
+        return a
+
+    def __next__(self):
+        file_list = self.file_list()
+        self.counter += 1
+        if self.counter < len(file_list):
+            return file_list[self.counter]
+        else:
+            raise StopIteration
+
