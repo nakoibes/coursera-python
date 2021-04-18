493555c70fd2db2b07ef5a37cd0655c5f52f0656
nakoibes <alefanow.d@yandex.ru>
2021-04-18 13:29:45 +0300

    client

--- first_course/week5/client.py
+++ first_course/week5/client.py
@@ -5,6 +5,12 @@ import time
 class ClientError(Exception):
     pass
 
+class ValidationError(Exception):
+    pass
+
+class TransportError(Exception):
+    pass
+
 
 class Client:
     def __init__(self, addr, port, timeout=None):
@@ -12,15 +18,17 @@ class Client:
 
     def get(self, name: str):  # -> dict[str, list[tuple[int, float]]]: Закоментил чтоб в курсеру сдать
         response = self._transport.perform_request('get', name)
-        status = response[0]
-        if status != 'ok':
-            raise ClientError('bad response status', ','.join(response[1::]))
-        data_dict = MetricConstructor(response).construct()
+        self.check_status(response)
+        data_dict = ResponseConstructor(response).construct()
         return data_dict
 
-    def put(self, name: str, value: float, timestamp: int = None) -> None:
+    def put(self, metric: str, value: float, timestamp: int = None) -> None:
         timestamp = timestamp or int(time.time())
-        response = self._transport.perform_request('put', name, value, timestamp)
+        response = self._transport.perform_request('put', metric, str(value), str(timestamp))
+        self.check_status(response)
+
+    @staticmethod
+    def check_status(response):
         status = response[0]
         if status != 'ok':
             raise ClientError('bad response status', ','.join(response[1::]))
@@ -34,23 +42,17 @@ class Transport:
         try:
             self.sock = socket.create_connection((addr, port))
         except socket.error as err:
-            raise ClientError('Cannont create connection', err) from err
+            raise TransportError('Cannont create connection', err) from err
         self.sock.settimeout(timeout)
 
     def read(self):
         return self.sock.recv(1024)
 
     def perform_request(self, method: str, *args):  # -> list[str]:
-        if method == 'put':
-            self.sock.send(bytes(f'put {args[0]} {args[1]} {args[2]}\n', encoding='utf-8'))
-            data_bytes = self.read()
-            data_list = Deserializer().loads(data_bytes)
-            return data_list
-        elif method == 'get':
-            self.sock.send(bytes(f'get {args[0]}\n', encoding='utf-8'))
-            data_bytes = self.read()
-            data_list = Deserializer().loads(data_bytes)
-            return data_list
+        self.sock.send(bytes(f'{method} {" ".join(args)}\n', encoding='utf-8'))
+        data_bytes = self.read()
+        data_list = Deserializer().loads(data_bytes)
+        return data_list
 
 
 class Deserializer:
@@ -63,18 +65,19 @@ class Deserializer:
         return data_list
 
 
-class MetricConstructor:
+class ResponseConstructor:
     def __init__(self, data_list):
         self.data_list = data_list[1:]
 
     def construct(self):  # -> tuple[str, dict[str, list[tuple[int, float]]]]:
         result = {}
-        for item in self.data_list:
-            result.setdefault(item.split()[0], [])
-            try:
-                tup = (int(item.split()[2]), float(item.split()[1]))
-            except:
-                raise ClientError('Cannont create connection')
-            result[item.split()[0]].append(tup)
-            result[item.split()[0]].sort()
+        try:
+            for item in self.data_list:
+                metric_name, metric_rate, metric_timestamp = item.split()
+                result.setdefault(metric_name, [])
+                tup = (int(metric_timestamp), float(metric_rate))
+                result[metric_name].append(tup)
+                result[metric_name].sort()
+        except:
+            raise ValidationError('Bad data')
         return result  # че то я пока не понял зачем статус возвращать
