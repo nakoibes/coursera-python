97a3f1ace739bde94eee6e3b6f366301499ef9a8
nakoibes <alefanow.d@yandex.ru>
2021-04-20 16:34:56 +0300

    server

--- first_course/week6/server.py
+++ first_course/week6/server.py
@@ -46,67 +46,44 @@ class CommandHandler:
     storage = Storage()
 
     def handle(self, data):
-        data_list = data.split()
-        method = data_list[0]
+        method, *params = data.split()
         if method == 'put':
-            storage_response = self.put_data(data_list)
-            return storage_response
+            key, value, timestamp = params
+            value, timestamp = float(value), int(timestamp)
+            self.storage.put(key, timestamp, value)
+            return {}
         elif method == 'get':
-            data_list = data_list[1:]
-            storage_response = self.get_data(data_list)
+            key = params.pop()
+            if params:
+                raise CommandHandlerError
+            storage_response = self.storage.get(key)
             return storage_response
         else:
             raise CommandHandlerError
 
-    def get_data(self, data_list):
-        key = data_list.pop()
-        if data_list:
-            raise CommandHandlerError
-        response_dict = self.storage.get(key)
-        return response_dict
-
-    def put_data(self, data_list):
-        # print(data_list)
-        key, value, timestamp = map(str, data_list[1:])
-        if self.validate(value, timestamp):
-            value = str(float(value))
-            self.storage.put(key, timestamp, value)
-            return {}
-        raise CommandHandlerError
-
-    @staticmethod
-    def validate(value, timestamp):
-        try:
-            float(value)
-            int(timestamp)
-            return True
-        except:
-            return False
-
 
 class ResoponseConstructor:
-    def __init__(self, response_data):
-        self.response_data = response_data
+    def __init__(self):
         self.ok = 'ok\n\n'
-        self.response = 'ok\n'
 
-    def make_response(self):
-
-        if self.response_data:
-            for key, timestamps in self.response_data.items():
+    def make_response(self, response_data):
+        response = 'ok\n'
+        if response_data:
+            for key, timestamps in response_data.items():
                 for timestamp, value in timestamps.items():
-                    self.response += f'{key} {value} {timestamp}\n'
-            self.response += '\n'
-            return self.response
+                    response += f'{key} {value} {timestamp}\n'
+            response += '\n'
+            return response
         else:
             return self.ok
 
 
 class ClientServerProtocol(asyncio.Protocol):
-    command_handler = CommandHandler()
+    error = 'error\nwrong command\n\n'
 
     def __init__(self):
-        self.error = 'error\nwrong command\n\n'
+        self.command_handler = CommandHandler()
+        self.response_constructor = ResoponseConstructor()
 
     def connection_made(self, transport: Transport) -> None:
         self.transport = transport
@@ -115,9 +92,11 @@ class ClientServerProtocol(asyncio.Protocol):
         data = data.decode()
         try:
             response_dict = self.command_handler.handle(data)
-            response = ResoponseConstructor(response_dict).make_response()
-        except (CommandHandlerError, ValueError, IndexError):
+            response = self.response_constructor.make_response(response_dict)
+        except (CommandHandlerError, ValueError, IndexError) as err:
+            print(err)
             response = self.error
+        # print(response)
         self.transport.write(response.encode('utf-8'))
 
 
