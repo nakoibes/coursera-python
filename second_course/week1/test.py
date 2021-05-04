45bb2d46342cce376b6bea01b5394fe2a65e9c19
nakoibes <alefanow.d@yandex.ru>
2021-05-04 12:09:44 +0300

    test

--- second_course/week1/test.py
+++ second_course/week1/test.py
@@ -5,49 +5,42 @@ from factorize import factorize
 
 class TestFactorize(unittest.TestCase):
     def test_wrong_types_raise_exception(self):
-        with self.subTest(x=1):
-            self.assertRaises(TypeError, factorize, 'string')
-        with self.subTest(x=2):
-            self.assertRaises(TypeError, factorize, 1.5)
+        test_cases = (1.5, 'string')
+        for x in test_cases:
+            with self.subTest(x=x):
+                with self.assertRaises(TypeError):
+                    factorize(x)
 
     def test_negative(self):
-        with self.subTest(x=1):
-            self.assertRaises(ValueError, factorize, -1)
-        with self.subTest(x=2):
-            self.assertRaises(ValueError, factorize, -10)
-        with self.subTest(x=3):
-            self.assertRaises(ValueError, factorize, -100)
+        test_cases = (-1, -10, -100)
+        for x in test_cases:
+            with self.subTest(x=x):
+                with self.assertRaises(ValueError):
+                    factorize(x)
 
     def test_zero_and_one_cases(self):
-        with self.subTest(x=1):
-            self.assertEqual(factorize(0), (0,))
-        with self.subTest(x=2):
-            self.assertEqual(factorize(1), (1,))
+        test_cases = (0, 1)
+        for x in test_cases:
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), (x,))
 
     def test_simple_numbers(self):
-        with self.subTest(x=1):
-            self.assertEqual(factorize(3), (3,))
-        with self.subTest(x=2):
-            self.assertEqual(factorize(13), (13,))
-        with self.subTest(x=3):
-            self.assertEqual(factorize(29), (29,))
+        test_cases = (3, 13, 29)
+        for x in test_cases:
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), (x,))
 
     def test_two_simple_multipliers(self):
-        with self.subTest(x=1):
-            self.assertEqual(factorize(6), (2, 3))
-        with self.subTest(x=2):
-            self.assertEqual(factorize(26), (2, 13))
-        with self.subTest(x=3):
-            self.assertEqual(factorize(121), (11, 11))
+        test_cases = ((6, (2, 3)), (26, (2, 13)), (121, (11, 11)))
+        for x, exp in test_cases:
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), exp)
 
     def test_many_multipliers(self):
-        with self.subTest(x=1):
-            self.assertEqual(factorize(1001), (7, 11, 13))
-        with self.subTest(x=2):
-            self.assertEqual(factorize(9699690), (2, 3, 5, 7, 11, 13, 17, 19))
-
-
-
+        test_cases = ((1001, (7, 11, 13), (9699690, (2, 3, 5, 7, 11, 13, 17, 19))))
+        for x, exp in test_cases:
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), exp)
 
 
 if __name__ == '__main__':
