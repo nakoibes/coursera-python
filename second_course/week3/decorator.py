9954f4dd17f3f341299feb7ecdadadcaa00bd936
nakoibes <alefanow.d@yandex.ru>
2021-05-10 21:53:15 +0300

    decorator

---
+++ second_course/week3/decorator.py
@@ -0,0 +1,150 @@
+from abc import ABC, abstractmethod
+
+#from hero import Hero
+
+
+class AbstractEffect(ABC, Hero):
+    @abstractmethod
+    def get_positive_effects(self):
+        pass
+
+    @abstractmethod
+    def get_stats(self):
+        pass
+
+
+class AbstractPositive(AbstractEffect):
+    def __init__(self, base):
+        self.base = base
+
+    @abstractmethod
+    def get_positive_effects(self):
+        pass
+
+    @abstractmethod
+    def get_negative_effects(self):
+        pass
+
+    @abstractmethod
+    def get_stats(self):
+        pass
+
+
+class AbstractNegative(AbstractEffect):
+    def __init__(self, base):
+        self.base = base
+
+    @abstractmethod
+    def get_negative_effects(self):
+        pass
+
+    @abstractmethod
+    def get_stats(self):
+        pass
+
+
+class Berserk(AbstractPositive):
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    def get_positive_effects(self):
+        # print('---')
+        tmp = self.base.get_positive_effects().copy()
+        tmp.append('Berserk')
+        # print(tmp)
+        return tmp
+
+    def get_stats(self):
+        tmp = self.base.get_stats().copy()
+        tmp['Strength'] += 7
+        tmp['Luck'] += 7
+        tmp['Agility'] += 7
+        tmp['Endurance'] += 7
+        tmp['Perception'] -= 3
+        tmp['Charisma'] -= 3
+        tmp['Intelligence'] -= 3
+        tmp['HP'] += 50
+        return tmp
+
+
+class Blessing(AbstractPositive):
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    def get_positive_effects(self):
+        tmp = self.base.get_positive_effects().copy()
+        tmp.append('Blessing')
+        return tmp
+
+    def get_stats(self):
+        tmp = self.base.get_stats().copy()
+        tmp['Strength'] += 2
+        tmp['Perception'] += 2
+        tmp['Endurance'] += 2
+        tmp['Charisma'] += 2
+        tmp['Intelligence'] += 2
+        tmp['Agility'] += 2
+        tmp['Luck'] += 2
+        return tmp
+
+
+class Weakness(AbstractNegative):
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        tmp = self.base.get_negative_effects().copy()
+        tmp.append('Weakness')
+        return tmp
+
+    def get_stats(self):
+        tmp = self.base.get_stats()
+        tmp['Strength'] -= 4
+        tmp['Endurance'] -= 4
+        tmp['Agility'] -= 4
+        return tmp
+
+
+class EvilEye(AbstractNegative):
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        tmp = self.base.get_negative_effects()
+        tmp.append('EvilEye')
+        return tmp
+
+    def get_stats(self):
+        tmp = self.base.get_stats()
+        tmp['Luck'] -= 10
+        return tmp
+
+
+class Curse(AbstractNegative):
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        tmp = self.base.get_negative_effects()
+        tmp.append('Curse')
+        return tmp
+
+    def get_stats(self):
+        tmp = self.base.get_stats()
+        tmp['Strength'] -= 2
+        tmp['Perception'] -= 2
+        tmp['Endurance'] -= 2
+        tmp['Charisma'] -= 2
+        tmp['Intelligence'] -= 2
+        tmp['Agility'] -= 2
+        tmp['Luck'] -= 2
+        return tmp
+
+# h = Hero()
+# # print(h.get_stats())
+# #a = AbstractEffect(h)
+# b = Berserk(h)
+# #b.get_positive_effects()
+# bl = Blessing(b)
+# print(bl.get_positive_effects())
+# print(bl.get_stats())
